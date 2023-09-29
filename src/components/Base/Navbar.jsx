8f42aa38a35ce94ea94d3741c11d8453c51d94af
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-09-30 00:13:42 +0600

    Add theme

--- src/components/Base/Navbar.jsx
+++ src/components/Base/Navbar.jsx
@@ -1,9 +1,27 @@
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        localStorage.setItem("theme", theme === "dark" ? "light" : "dark");
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+    // initially set the theme and "listen" for changes to apply them to the HTML tag
+    useEffect(() => {
+        const localTheme = localStorage.getItem("theme");
+        localTheme && setTheme(localTheme);
+        localTheme &&
+            document
+                .querySelector("html")
+                .setAttribute("data-theme", localTheme);
+    }, [theme]);
+
     return (
         <nav className="w-full fixed top-0 shadow-xl px-5 z-50">
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 flex-col md:flex-col lg:flex-row">
                 <div className="flex-1 text-xl">
                     <img className="w-10 h-10" src="logo.png" alt="" />
                     <NavLink to="/">Kindness Collective</NavLink>
@@ -66,6 +84,23 @@ const Navbar = () => {
                                 About
                             </NavLink>
                         </li>
+                        <div className="">
+                            <label className="swap swap-rotate">
+                                <input onClick={toggleTheme} type="checkbox" />
+                                <div className="swap-on">
+                                    <Icon
+                                        className="text-xl"
+                                        icon="line-md:moon-loop"
+                                    />
+                                </div>
+                                <div className="swap-off">
+                                    <Icon
+                                        className="text-xl"
+                                        icon="line-md:sun-rising-loop"
+                                    />
+                                </div>
+                            </label>
+                        </div>
                     </ul>
                 </div>
             </div>
