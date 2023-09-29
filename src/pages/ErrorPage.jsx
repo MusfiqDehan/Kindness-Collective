74bf3b930eb7419b451617b4dcccc2b948947b0e
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-09-30 01:07:33 +0600

    Update error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen text-center">
+            <img
+                className="w-96"
+                src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+                alt="404"
+            />
             <h1 className="text-5xl font-bold mb-10">404 Error!</h1>
             <h2 className="text-3xl mb-10">Page not found!</h2>
             <Link className="btn btn-primary" to="/">
