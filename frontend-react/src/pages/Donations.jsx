36ae81ede2d889c81921e6588035d2767658d970
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-10-04 02:32:25 +0600

    Add loading

--- frontend-react/src/pages/Donations.jsx
+++ frontend-react/src/pages/Donations.jsx
@@ -3,13 +3,14 @@ import Card from "../components/Donation/Card";
 
 const Donations = () => {
     const [favorites, setFavorites] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [noFound, setNofound] = useState(false);
     const [isShow, setIsShow] = useState(false);
 
     useEffect(() => {
         const favoriteItems = JSON.parse(localStorage.getItem("favorites"));
 
-        if (favoriteItems.length > 0) {
+        if (favoriteItems && favoriteItems.length > 0) {
             setFavorites(favoriteItems);
 
             const total = favoriteItems.reduce(
@@ -21,6 +22,8 @@ const Donations = () => {
         } else {
             setNofound("No Data Found");
         }
+
+        setIsLoading(false);
     }, []);
 
     const handleDelete = (id) => {
@@ -28,6 +31,14 @@ const Donations = () => {
         setFavorites(newFavorites);
         localStorage.setItem("favorites", JSON.stringify(newFavorites));
     };
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
+        );
+    }
 
     return (
         <div>
