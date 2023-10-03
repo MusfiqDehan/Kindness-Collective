cf8c64644128d6131f4c5eb6e59a14b5a5a837f3
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-10-04 01:13:05 +0600

    Add Loading animation

--- frontend-react/src/components/Home/CardsContainer.jsx
+++ frontend-react/src/components/Home/CardsContainer.jsx
@@ -1,19 +1,31 @@
 import Card from "./Card";
+import { useState, useEffect } from "react";
 
 const CardsContainer = ({ cards, searchResults }) => {
-    console.log(cards);
-    return searchResults.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-20 justify-items-center">
-            {searchResults.map((card) => {
-                return <Card key={card.id} card={card} />;
-            })}
-        </div>
-    ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-20 justify-items-center">
-            {cards.map((card) => {
-                return <Card key={card.id} card={card} />;
-            })}
-        </div>
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, []);
+
+    return (
+        <>
+            {isLoading ? (
+                <div className="flex justify-center items-center h-screen">
+                    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-20 justify-items-center">
+                    {searchResults.length > 0
+                        ? searchResults.map((card) => {
+                              return <Card key={card.id} card={card} />;
+                          })
+                        : cards.map((card) => {
+                              return <Card key={card.id} card={card} />;
+                          })}
+                </div>
+            )}
+        </>
     );
 };
 
